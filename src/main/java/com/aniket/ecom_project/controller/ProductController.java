aeccdc7b638abfe8cdbb20fc19f627f3c45f1324
aniket0880 <aabaravkar456@gmail.com>
2025-03-02 01:28:38 +0530

    Response entity.

--- src/main/java/com/aniket/ecom_project/controller/ProductController.java
+++ src/main/java/com/aniket/ecom_project/controller/ProductController.java
@@ -1,15 +1,37 @@
 package com.aniket.ecom_project.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.aniket.ecom_project.model.Products;
+import com.aniket.ecom_project.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class ProductController {
 
-    @RequestMapping
-    public String greet(){
-        return "HELLO WORLD";
+    @Autowired
+    private ProductService service;
+
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Products>> getAllProducts() {
+
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
     }
 
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Products> getProduct(@PathVariable int id) {
+        Products product = service.getProduct(id);
+        if (product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- src/main/java/com/aniket/ecom_project/model/Products.java
+++ src/main/java/com/aniket/ecom_project/model/Products.java
@@ -1,7 +1,6 @@
 package com.aniket.ecom_project.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,11 +9,12 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 @Entity
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class Products {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String desc;
@@ -25,4 +25,75 @@ public class Products {
     private boolean available;
     private int quantity;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
