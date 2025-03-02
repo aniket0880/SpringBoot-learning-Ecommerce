6e07431feeccccdfd66d4774bc9c8af2821860fe
aniket0880 <aabaravkar456@gmail.com>
2025-03-03 00:27:01 +0530

    Product update and delete method implemented

--- src/main/java/com/aniket/ecom_project/controller/ProductController.java
+++ src/main/java/com/aniket/ecom_project/controller/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 
@@ -57,5 +58,39 @@ public class ProductController {
         byte[] imageFile=products.getImageData();
 
         return ResponseEntity.ok().contentType(MediaType.valueOf(products.getImageType())).body(imageFile);
+    }
+
+    @PutMapping("/product/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable int id ,
+        @RequestPart Products products,
+        @RequestPart MultipartFile imageFile)  {
+
+        Products products1=null;
+        try {
+
+            products1 = service.updateProduct(id, products, imageFile);
+        } catch(Exception e){
+            return new ResponseEntity<>("Failed to update",HttpStatus.BAD_REQUEST);
+        }
+        if(products!=null)
+            return new ResponseEntity<>("Updated",HttpStatus.OK);
+
+        else{
+            return new ResponseEntity<>("Failed to update",HttpStatus.BAD_REQUEST);
+        }
+
+
+    }
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id){
+
+        Products products=service.getProduct(id);
+        if(products!=null){
+            service.deleteProduct(id);
+            return new ResponseEntity<>("Deleted",HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>("Product Not Found",HttpStatus.NOT_FOUND);
+        }
     }
 }
